62a88287f99a512abf4c5c21d39314a77c890064
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2021-04-30 14:08:39 +0000

    Gestion des tickets

---
+++ src/Controller/AdminTicketController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ticket;
+use App\Repository\TicketRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AdminTicketController extends AbstractController
+{
+    const menu = "gestion";
+    const sub_menu = "ticket";
+
+    /**
+     * @Route("/admin/ticket", name="admin_ticket")
+     */
+    public function index(TicketRepository $ticketRepository): Response
+    {
+        // S'il n'y a aucun ticket alors generer les tickets
+        $tickets = $ticketRepository->findAll();
+
+        if (!$tickets) {
+            $this->ticketGenerated();
+
+            $this->addFlash('success', "Les tickets ont été générés avec succès!");
+            $tickets = $ticketRepository->findAll();
+        }
+
+        return $this->render('admin_ticket/index.html.twig', [
+            'tickets' => $tickets,
+            'menu' => self::menu,
+            'sub_menu' => self::sub_menu
+        ]);
+    }
+
+    /**
+     * Generation des code de tickets
+     *
+     * @return bool
+     */
+    protected function ticketGenerated(): bool
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        for ($i = 1; $i <= 200; $i++)
+        {
+            $ticket = new Ticket();
+            $code = $this->codification($i);
+            $ticket->setCode($code);
+            $ticket->setPrix(150000);
+            $ticket->setMedia($code.'.png');
+
+            $em->persist($ticket);
+        }
+
+        $em->flush();
+
+        return true;
+    }
+
+    /**
+     * Generation de codes
+     * @param $i
+     * @return string
+     */
+    protected function codification($i): String
+    {
+        if ($i < 10){
+            $code = 'GLB2100'.$i;
+        }elseif ($i < 100){
+            $code = 'GLB210'.$i;
+        }else{
+            $code = 'GLB21'.$i;
+        }
+
+        return $code;
+    }
+}
---
+++ src/Entity/Ticket.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TicketRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TicketRepository::class)
+ */
+class Ticket
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $code;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $prix;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $remise;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $media;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $statut;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $place;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCode(): ?string
+    {
+        return $this->code;
+    }
+
+    public function setCode(?string $code): self
+    {
+        $this->code = $code;
+
+        return $this;
+    }
+
+    public function getPrix(): ?int
+    {
+        return $this->prix;
+    }
+
+    public function setPrix(?int $prix): self
+    {
+        $this->prix = $prix;
+
+        return $this;
+    }
+
+    public function getRemise(): ?int
+    {
+        return $this->remise;
+    }
+
+    public function setRemise(?int $remise): self
+    {
+        $this->remise = $remise;
+
+        return $this;
+    }
+
+    public function getMedia(): ?string
+    {
+        return $this->media;
+    }
+
+    public function setMedia(?string $media): self
+    {
+        $this->media = $media;
+
+        return $this;
+    }
+
+    public function getStatut(): ?bool
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(?bool $statut): self
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+
+    public function getPlace(): ?string
+    {
+        return $this->place;
+    }
+
+    public function setPlace(?string $place): self
+    {
+        $this->place = $place;
+
+        return $this;
+    }
+}
